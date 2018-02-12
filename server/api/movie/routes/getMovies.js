7d5a449c9614a0b57d4937dfdb70188708393673
Steve Lebleu <info@e-lless.be>
2018-02-12 16:01:46 +0100

    fixed get movies

--- server/api/movie/routes/getMovies.js
+++ server/api/movie/routes/getMovies.js
@@ -1,5 +1,10 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-    
+    method: 'GET',
+    path: '/get/',
+    handler: async (request, reply)=> {
+        let movies = await Movie.find();
+        return movies;  
+    }
 }
