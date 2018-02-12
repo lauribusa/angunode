5601d842c8a01b6d37981c7bcea1d170ff97a26c
Steve Lebleu <info@e-lless.be>
2018-02-12 16:11:26 +0100

    fixed get movies

--- server/api/movie/routes/getMovieByID.js
+++ server/api/movie/routes/getMovieByID.js
@@ -2,9 +2,9 @@ const Movie = require('../models/movie');
 
 module.exports = {
     method: 'GET',
-    path: '/get/:id',
+    path: '/get/{id}',
     handler: async (request, reply)=> {
-        let movie = await Movie.find(id: req.params.id);
+        let movie = await Movie.find({id: req.params.id});
         return movie;  
     }
 }
