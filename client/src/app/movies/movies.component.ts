9f36fb81643701b5f1b8cd573f06ca4d746389c7
Lauri <lauri.busa@gmail.com>
2018-02-13 11:36:40 +0100

    ngFor now works

--- client/src/app/movies/movies.component.ts
+++ client/src/app/movies/movies.component.ts
@@ -7,14 +7,26 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-
-  constructor(public http: HttpClient) {}
-
-  ngOnInit() {
-    this.http.get('http://localhost:9000/api/movies')
-    .toPromise()
-    .then(result => console.log(result))
-    .catch(err=> console.log(err));
+  constructor(public http: HttpClient) {
+    
   }
+  films: any = []
+  ngOnInit() {
+    this.getMovie();
+    // this.http.get('http://localhost:9000/api/movies')
+    // .toPromise()
+    // .then(result =>{
+    //   result.map(res => console.log(res.original_title));
+    //   console.log(result);
+    // })
+    // .catch(err=> console.log(err));
+  }
+
+  getMovie() {
+   return this.http.get('http://localhost:9000/api/movies').toPromise().then(result => {
+    //result.map(res => console.log(res.original_title));
+    this.films = result;
+   }).catch(err => console.log(err));
+ }
 
 }
