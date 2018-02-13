362e700e734d400f96929cc651abcc667eb4a27c
Lauri <lauri.busa@gmail.com>
2018-02-13 09:53:28 +0100

    fixed cors, added HttpModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
