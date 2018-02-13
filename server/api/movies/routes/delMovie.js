9fee7145766d462b85dc8f29b0e5d78049ed7c3a
nanosix <nanosix@gmail.com>
2018-02-13 09:26:16 +0100

    connard de commit

--- server/api/movies/routes/delMovie.js
+++ server/api/movies/routes/delMovie.js
@@ -3,7 +3,7 @@ module.exports = {
     method: 'DELETE',
     path: '/api/movies/{id}',
     handler: async (req, rep)=>{
-        await Movie findOneAndRemove({id: req.params.id});
+        await Movie.findOneAndRemove({id: req.params.id});
         return 'movie deleted';
     }
 }
