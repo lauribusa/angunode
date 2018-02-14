172c6d0d442de4bb156a1d97effa787945d7f60e
Lauri <lauri.busa@gmail.com>
2018-02-14 10:26:30 +0100

    sauce

--- client/src/app/post/post.component.ts
+++ client/src/app/post/post.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders} from '@angular/core';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -7,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+    this.postMovies(this.body);
+  }
+
+  postMovies(hero){
+    return this.http.post('http://localhost:9000/api/movies', body, any)
+    .toPromise()
+    .then(result => { 
+      console.log(result);
+    })
+    .catch(err => console.log(err));
   }
 
 }
